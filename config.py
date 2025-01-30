a59052aa6c158eab5f0c8f8bda7cc3c5d65143b3
Vignesh1050 <iit2021262@iiita.ac.in>
2025-01-30 13:59:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 DATABASE_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'Vignesh@0305',  # Change this to your MySQL password
+    'password': 'Vignesh@0305', 
     'database': 'VRCTC'
 }
 
-SECRET_KEY = "030503"  # Change this to a strong secret key
-ADMIN_API_KEY = "102703"  # Change this to a secure admin API key
+SECRET_KEY = "030503"  
+ADMIN_API_KEY = "102703"  
